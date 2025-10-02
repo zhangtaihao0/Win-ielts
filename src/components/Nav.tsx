1e00e62fa57ddad79622097b173d1d92806676c4
joeldeondsouza <jdeondsouza@gmail.com>
2025-10-02 10:52:35 +0200

    chore: configure Husky hooks and add styled-components

---
+++ src/components/Nav.tsx
@@ -0,0 +1,18 @@
+import { NavContainer, NavContent, Logo, DetailsContainer, NavTextLink } from './NavStyled';
+// Logo //
+import logo from '/logo.png';
+
+const Nav = () => {
+  return (
+    <NavContainer>
+      <NavContent>
+        <Logo src={logo} alt="Logo" />
+        <DetailsContainer>
+          <NavTextLink href="">GitHub</NavTextLink>
+        </DetailsContainer>
+      </NavContent>
+    </NavContainer>
+  );
+};
+
+export default Nav;
---
+++ src/components/NavStyled.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const NavContainer = styled.nav`
+  height: 70px;
+  display: flex;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 960px) {
+    transition: 0.9s all ease;
+  }
+`;
+
+export const NavContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 1;
+  width: 100%;
+  padding: 15px 0px;
+  max-width: 1060px;
+  margin-left: 10px;
+  margin-right: 10px;
+  @media screen and (max-width: 960px) {
+    padding: 15px 10px;
+  }
+`;
+
+export const Logo = styled.img`
+  width: 48px;
+  height: 48px;
+  @media screen and (max-width: 960px) {
+    height: 40px;
+  }
+`;
+
+export const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const NavTextLink = styled.a`
+  text-decoration: none;
+  font-size: 15px;
+  font-weight: 500;
+  cursor: pointer;
+  padding: 0 10px;
+  color: #090040;
+  &:hover {
+    color: #346751;
+    text-decoration: underline;
+    text-underline-offset: 5px;
+  }
+`;
