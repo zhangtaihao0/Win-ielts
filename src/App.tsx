9f9e2b652416cbd164a9175ef13acbd50c54d064
joeldeondsouza <jdeondsouza@gmail.com>
2025-10-03 11:49:25 +0200

    fix: restore HashRouter rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // Router //
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home, Initiate } from './pages';
 
 function App() {
