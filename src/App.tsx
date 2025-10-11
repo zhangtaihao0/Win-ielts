11f919369294505a6d1050aa21bf83ab4328b5f6
joeldeondsouza <jdeondsouza@gmail.com>
2025-10-11 02:18:05 +0200

    feat: preload images and improve rendering performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
+import { useEffect, useState } from 'react';
 // Router //
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home, Initiate, ReadingTest, WritingTest, ListeningTest, Results } from './pages';
+// styles //
+import { LoadingBlock, LoadingGif } from './styles';
+// imgs //
+import listeningImg from '/img/listening.png';
+import readingImg from '/img/reading.png';
+import writingImg from '/img/writing.png';
+import speakingImg from '/img/speaking.png';
+import trophyImg from '/img/trophy.png';
+// Loader //
+import Loader from '/img/loading.gif';
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Preload all images //
+    const imageSources = [listeningImg, readingImg, writingImg, speakingImg, trophyImg];
+    // img loaded //
+    let loadedImages = 0;
+    imageSources.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => {
+        loadedImages++;
+        // Check if all images and font are loaded //
+        if (loadedImages === imageSources.length) {
+          setIsLoaded(true);
+        }
+      };
+    });
+  }, []);
+
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/initiate" element={<Initiate />} />
-          <Route path="/reading-test" element={<ReadingTest />} />
-          <Route path="/writing-test" element={<WritingTest />} />
-          <Route path="/listening-test" element={<ListeningTest />} />
-          <Route path="/results" element={<Results />} />
-        </Routes>
-      </Router>
+      {!isLoaded ? (
+        <LoadingBlock>
+          <LoadingGif src={Loader} alt="Loading..." />
+        </LoadingBlock>
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/initiate" element={<Initiate />} />
+            <Route path="/reading-test" element={<ReadingTest />} />
+            <Route path="/writing-test" element={<WritingTest />} />
+            <Route path="/listening-test" element={<ListeningTest />} />
+            <Route path="/results" element={<Results />} />
+          </Routes>
+        </Router>
+      )}
     </>
   );
 }
